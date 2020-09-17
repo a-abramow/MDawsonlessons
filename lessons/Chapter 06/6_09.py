466d65701d37bd28327bd9ec6f29af6b8c1da484
Andrey <abramow0000@yandex.ru>
2020-09-17 17:32:40 +0300

    6_09

---
+++ lessons/Chapter 06/6_09.py
@@ -0,0 +1,70 @@
+# Крестики - нолики
+# Компьютер играет в крестики - нолики против пользователя
+# глобальные константы
+X = "X"
+O = "O"
+EMPTY = ""
+TIE = "Ничья"
+NUM_SQUARES = 9
+
+
+def display_instruct():
+    print(
+        """
+        Добро пожаловать на ринг игры Крестики Нолики.
+        Чтобы сделать ход, ыыедите число от 0 до 8. Числа
+        соответствуют полям, как указано ниже:
+        0 | 1 | 2
+        ---------
+        3 | 4 | 5
+        ---------
+        6 | 7 | 8
+        Приготовься к бою, жалкий людишка. Познай силу процессора!\n    """
+    )
+
+
+def ask_yes_no(question):
+    """Задает вопрос с ответом 'да' или 'нет'."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+
+def ask_number(question, low, high):
+    """Просит ввести число из диапазона."""
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+
+def pieces():
+    """Определяет принадлежность первого хода."""
+    go_first = ask_yes_no("Хочешь оставить за собой право первого хода? (y/n): ")
+    if go_first == "y":
+        print("\nНу что ж, даю тебе фору: играй крестиками.")
+        human = X
+        computer = O
+    else:
+        print("\nТвоя удаль тебя погубит... Буду начинать я.")
+        computer = X
+        human = O
+        return computer, human
+
+
+def new_board():
+    """Создает новую игровую доску."""
+    board = []
+    for square in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
+
+
+def display_board(board):
+    """Отображает игровую доску на экране."""
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\n\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\n\t", board[6], "|", board[7], "|", board[8], "\n")
