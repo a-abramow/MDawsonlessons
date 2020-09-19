b8dec6d045ee09cb01f7ea9204c9da0f3a747a3c
Andrey <abramow0000@yandex.ru>
2020-09-19 15:29:15 +0300

    7_01

--- lessons/Chapter 07/7_01.py
+++ lessons/Chapter 07/7_01.py
@@ -44,4 +44,4 @@ text_file = open("read_it.txt", "r", encoding='utf-8')
 for line in text_file:
     print(line)
 text_file.close()
-input("\n\nНажмите Enter, чтобы выйти.")
+input("\nНажмите Enter, чтобы выйти.")
