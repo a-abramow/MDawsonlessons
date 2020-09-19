5c6b39f27ecb7746fae286472c090f6dfb82d976
Andrey <abramow0000@yandex.ru>
2020-09-19 16:26:09 +0300

    7_02

--- lessons/Chapter 07/7_02.py
+++ lessons/Chapter 07/7_02.py
@@ -7,10 +7,22 @@ text_file = open("write_it.txt", "w", encoding='utf-8')
 text_file.write("Первая строка номер 1\n")
 text_file.write("Вторая строка номер 2\n")
 text_file.write("Третья строка номер 3\n")
-text_file.write("Первая строка номер 1\nВторая строка номер 2\nТретья строка номер 3\n")
-
 text_file.close()
+
 print("\nЧитаю вновь созданный файл.")
 text_file = open("write_it.txt", "r", encoding='utf-8')
+print(text_file.read())
+text_file.close()
+
+print("Создаю текстовый файл методом writelines().")
+text_file = open("write_it2.txt", "w", encoding='utf-8')
+lines = ["Строка номер 1\n"
+         "Строка номер 2\n"
+         "Строка номер 3\n"]
+text_file.writelines(lines)
+text_file.close()
+
+print("\nЧитаю вновь созданный файл.")
+text_file = open("write_it2.txt", "r", encoding='utf-8')
 print(text_file.read())
 text_file.close()
