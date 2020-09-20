2b758482efc2ec89de12cad7cd8efcde4f495dff
Andrey <abramow0000@yandex.ru>
2020-09-20 13:26:16 +0300

    7_04

--- lessons/Chapter 07/7_04.py
+++ lessons/Chapter 07/7_04.py
@@ -3,3 +3,14 @@
 
 import pickle
 import shelve
+
+print("Консервация списков.")
+variety = ["огурцы", "помидоры", "капуста"]
+shape = ["целые", "кубиками", "соломкой"]
+brand = ["Главпродукт", "Чумак", "Бондюэль"]
+f = open("pickles1.dat", "wb")
+
+pickle.dump(variety, f)
+pickle.dump(shape, f)
+pickle.dump(brand, f)
+f.close()
