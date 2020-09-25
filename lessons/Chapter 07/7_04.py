f3b29b88e903b18cd8c39c3492841d86c22bd38b
Andrey <abramow0000@yandex.ru>
2020-09-25 17:27:46 +0300

    7_04

--- lessons/Chapter 07/7_04.py
+++ lessons/Chapter 07/7_04.py
@@ -14,3 +14,30 @@ pickle.dump(variety, f)
 pickle.dump(shape, f)
 pickle.dump(brand, f)
 f.close()
+
+print("Расконсервация списков.")
+f = open("pickles1.dat", "rb")
+variety = pickle.load(f)
+shape = pickle.load(f)
+brand = pickle.load(f)
+
+print(variety)
+print(shape)
+print(brand)
+f.close()
+
+print("\nПомещение списков на полку.")
+s = shelve.open("pickles2.dat")
+s["variety"] = ["огурцы", "помидоры", "капуста"]
+s["shape"] = ["целые", "кубиками", "соломкой"]
+s["brand"] = ["Главпродукт", "Чумак", "Бондюэль"]
+
+s.sync()
+
+print("\nИзвлечение списков из файла полки:")
+print("тогровые марки - :", s["brand"])
+print("формы - :", s["shape"])
+print("виды овощей - :", s["variety"])
+s.close()
+input("\nНажмите Enter, чтобы выйти.")
+
