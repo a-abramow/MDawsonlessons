5064fd979cd2177f68dbbabe019777e174c22716
Andrey <abramow0000@yandex.ru>
2020-10-07 06:43:55 +0300

    8_01

--- lessons/Chapter 08/8_01.py
+++ lessons/Chapter 08/8_01.py
@@ -5,11 +5,16 @@
 class Critter(object):
     """Виртуальный питомец"""
 
+    def __init__(self):
+        print("Появилась на свет новая зверюшка.")
+
     def talk(self):
         print("Привет. Я зверюшка - экземпляр класса Critter.")
 
 
 # Основная часть
-crit = Critter()
-crit.talk()
+crit1 = Critter()
+crit2 = Critter()
+crit1.talk()
+crit2.talk()
 input("\n\nНажмите Enter, чтобы выйти.")
