ff6652d555bd04790cc9f69f731a5123ff6529ca
Andrey <abramow0000@yandex.ru>
2020-10-13 10:15:11 +0300

    SG

--- lessons/Chapter 09/games.py
+++ lessons/Chapter 09/games.py
@@ -9,25 +9,30 @@ class Player(object):
         self.name = name
         self.score = score
 
-    def __str__(self):
-        rep = self.name + "\t" + str(self.score)
-        return rep
 
-    def ask_yes_no(self, question):
-        """Задает вопрос с ответом 'да' или 'нет'."""
-        response = None
-        while response not in ("y", "n"):
-            response = input(question).lower()
-        return response
+def __str__(self):
+    rep = self.name + ":\t" + str(self.score)
+    return rep
 
-    def ask_number(self, question, low, high):
-        """Прости ввести число из заданного диапазона."""
-        response = None
-        while response not in range(low, high):
-            response = int(input(question))
-        return response
-    if __name__ == "__main__":
-        print("Вы запустили этот модуль напрямую, а не импортировали его.")
-        input("\n\nНажмите Enter, чтобы выйти.")
+
+def ask_yes_no(question):
+    """Задает вопрос с ответом 'да' или 'нет'."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+
+def ask_number(question, low, high):
+    """Прости ввести число из заданного диапазона."""
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+
+if __name__ == "__main__":
+    print("Вы запустили этот модуль напрямую, а не импортировали его.")
+    input("\n\nНажмите Enter, чтобы выйти.")
 
 
