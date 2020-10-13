e31047d90640f8708f7f17884f488f941982e9e0
Andrey <abramow0000@yandex.ru>
2020-10-13 08:07:22 +0300

    9_03

--- lessons/Chapter 09/9_03.py
+++ lessons/Chapter 09/9_03.py
@@ -43,12 +43,59 @@ class Hand(object):
         other_hand.add(card)
 
 
-class Desk(Hand):
+class Deck(Hand):
     """Колода игральных карт"""
 
+    def populate(self):
+        self.cards = []
+        for suit in Card.SUITS:
+            for rank in Card.RANKS:
+                self.add(Card(rank, suit))
+
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
+
+    def deal(self, hands, per_hand=1):
+        for rounds in range(per_hand):
+            for hand in hands:
+                if self.cards:
+                    top_card = self.cards[0]
+                    self.give(top_card, hand)
+                else:
+                    print("Не могу больше сдавать: карты закончились!")
 
 
 # основная часть
+deck1 = Deck()
+print("Создана новая колода.")
+print("Вот эта колода:")
+print(deck1)
+deck1.populate()
+print("В колоде появились карты.")
+print("Вот как колода выглядит теперь:")
+print(deck1)
+deck1.shuffle()
+print("Перетасуем карты в колоде")
+print("Вот как они теперь выглядят: ")
+print(deck1)
+my_hand = Hand()
+your_hand = Hand()
+hands = [my_hand, your_hand]
+deck1.deal(hands, per_hand=5)
+print("\nМне и Вам на руки выдано по 5 карт.")
+print("\nВот, что у Вас на руках.")
+print(your_hand)
+print("\nВот, что у меня на руках.")
+print(my_hand)
+print("\nОсталось в колоде:")
+print(deck1)
+deck1.clear()
+print("\nКолода очищена")
+print("Вот как она выглядит теперь:", deck1)
+input("\n\nНажмите Enter, чтобы выйти.")
+
+
 
 
 # card1 = Card(rank="Т", suit="к")
