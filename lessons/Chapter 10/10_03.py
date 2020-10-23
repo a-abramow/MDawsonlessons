7071c0cabb54d1c20250a327d91fe6b7f4089ed2
Andrey <abramow0000@yandex.ru>
2020-10-23 12:42:40 +0300

    10_03

--- lessons/Chapter 10/10_03.py
+++ lessons/Chapter 10/10_03.py
@@ -3,9 +3,23 @@
 
 from tkinter import *
 
+
 class Application(Frame):
     """GUI-приложение, которое подсчитывает количество нажатий кнопок"""
+
     def __init__(self, master):
         """Инициализирует рамку"""
         super(Application, self).__init__(master)
         self.grid()
+        self.bttn_clicks = 0  # количество нажатий
+        self.create_widget()
+
+    def create_widget(self):
+        self.bttn = Button(self)
+        self.bttn["text"] = "Количество щелчков: 0"
+        self.bttn["command"] = self.update_count
+        self.bttn.grid()
+
+    def update_count(self):
+        self.bttn_clicks += 1
+        self.bttn["text"] = "Количество щелчков: " + str(self.bttn_clicks)
