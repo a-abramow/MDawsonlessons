924c2b607ddf25536481a3f91d910e2548c05d91
Andrey <abramow0000@yandex.ru>
2020-10-24 21:04:14 +0300

    10_05

--- lessons/Chapter 10/10_05.py
+++ lessons/Chapter 10/10_05.py
@@ -17,13 +17,37 @@ class Application(Frame):
         """Создает элементы, с помощью которых пользователь будет выбирать."""
         # метка описание
         Label(self,
-              text = "Укажите Ваши любимые жанры в кино"
+              text="Укажите Ваши любимые жанры в кино"
               ).grid(row=0, column=0, sticky=W)
 
         # метка инструкция
         Label(self,
               text="Выберите все, что вам по вкусу:"
               ).grid(row=1, column=0, sticky=W)
+
+        # флажок "Комедия"
+        self.likes_comedy = BooleanVar()
+        Checkbutton(self,
+                    text="Комедия",
+                    variable=self.likes_comedy,
+                    command=self.update_text
+                    ).grid(row=2, column=0, sticky=W)
+        # флажок "Драма"
+        self.likes_drama = BooleanVar()
+        Checkbutton(self,
+                    text="Комедия",
+                    variable=self.likes_drama,
+                    command=self.update_text
+                    ).grid(row=3, column=0, sticky=W)
+
+        # флажок "Фильм о любви"
+        self.likes_romance = BooleanVar()
+        Checkbutton(self,
+                    text="Комедия",
+                    variable=self.likes_romance,
+                    command=self.update_text
+                    ).grid(row=4, column=0, sticky=W)
+
 
 root = Tk()
 root.title("Долгожитель")
