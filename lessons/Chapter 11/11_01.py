0eb7b86500ffdb29f459d2ebaf7742d7f543fcd6
Andrey <abramow0000@yandex.ru>
2020-10-26 14:42:52 +0300

    10_06

---
+++ lessons/Chapter 11/11_01.py
@@ -0,0 +1,8 @@
+# новое графическое окно
+# Демонстрирует создание графического окна
+
+from superwires import games
+
+games.init(screen_width=640, screen_height=480, fps=50)
+
+games.screen.mainloop()
